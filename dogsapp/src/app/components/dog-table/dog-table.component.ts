4d9b6bb4065a707d4b4cd4c71497aa5b0cf5d85d
Maria Camila <maria-alvarezp@unilibre.edu.co>
2025-06-25 06:59:40 -0500

    dog table created

--- dogsapp/src/app/components/dog-table/dog-table.component.ts
+++ dogsapp/src/app/components/dog-table/dog-table.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Dog, DogService } from '../../services/dog.service';
+import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dog-table',
-  imports: [],
+  imports: [MatCardModule, MatIconModule, RouterLink],
   templateUrl: './dog-table.component.html',
   styleUrl: './dog-table.component.css'
 })
 export class DogTableComponent {
+  private service: DogService = inject(DogService);
+  dogs = signal<Dog[]>([]);
+
+  ngOnInit() {
+    this.service.getDogs().subscribe({
+      next: values => { this.dogs.set(values) },
+      error: err => { console.log(err) }
+    });
+  }
+
+  deleteDog(id: number) {
+    this.service.deleteDog(id).subscribe({
+      error: err => { console.log(err) }
+    });
+  }
 
 }
